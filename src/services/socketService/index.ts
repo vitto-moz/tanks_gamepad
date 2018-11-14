b5e7e35b31b4a3538dac60d0b94b3b0de50fadad
vitto <v.mozharovsky@gmail.com>
2018-11-14 18:28:01 +0200

    socketService added

---
+++ src/services/socketService/index.ts
@@ -0,0 +1,20 @@
+import * as socketIo from "socket.io-client";
+import {ITanks} from './interfaces';
+import SOCKET_EVENTS from './socketEvents';
+
+const SERVER_URL = "http://localhost:8080";
+
+export class SocketService {
+  private socket: SocketIOClient.Socket;
+
+  constructor() {
+    this.socket = socketIo(SERVER_URL);
+  }
+
+  public registerUser(name: string) {
+    this.socket.emit(SOCKET_EVENTS.REGISTER_USER, name);
+  }
+}
+
+const socketService = new SocketService()
+export default socketService
---
+++ src/services/socketService/socketEvents.ts
@@ -0,0 +1,5 @@
+const SOCKET_EVENTS = {
+    REGISTER_USER: 'REGISTER_USER' 
+}
+
+export default SOCKET_EVENTS
